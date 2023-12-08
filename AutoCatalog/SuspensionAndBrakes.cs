9595f18d5cdc1a68d848191aa69495fd0800638f
nsstnc <egorgolubev0484@gmail.com>
2023-12-08 22:33:51 +0300

    Add Suspension and Brakes class

--- AutoCatalog/SuspensionAndBrakes.cs
+++ AutoCatalog/SuspensionAndBrakes.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AutoCatalog
 {
+    // класс описывающий подвеску и тормоза автомобиля
     internal class SuspensionAndBrakes
     {
+        public string TypeOfFrontSuspension { get; }
+        public string TypeOfBackSuspension { get; }
+        public string FrontBrakes { get; }
+        public string BackBrakes { get; }
+        public SuspensionAndBrakes(string typeOfFrontSuspension, string typeOfBackSuspension,
+            string frontBrakes, string backBrakes) 
+        { 
+            TypeOfFrontSuspension = typeOfFrontSuspension;
+            TypeOfBackSuspension = typeOfBackSuspension;
+            FrontBrakes = frontBrakes;
+            BackBrakes = backBrakes;
+        }
     }
 }
